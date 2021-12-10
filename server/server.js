a6d4d4090512b321123f7a4716d6a9f9061eadaa
Gabukuro <scatamburlo.gabriel@gmail.com>
2021-12-10 19:59:02 -0300

    adicionando serialport para comunicar com Arduino

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const Pool = require('pg').Pool;
+const cors = require('cors')
+const SerialPort = require('serialport');
+
 pool = new Pool({
   user: ''
 })
@@ -7,6 +10,17 @@ pool = new Pool({
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const arduinoCOMPort = "COM3";
+
+var arduinoSerialPort = new SerialPort(arduinoCOMPort, {  
+  baudRate: 9600
+});
+
+arduinoSerialPort.on('open',function() {
+  console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+});
 
 const DoorController = require('./src/controllers/DoorController');
 
@@ -17,11 +31,13 @@ app.get("/status/:doorID", (req, res) => {
 
 app.post("/open/:doorID", (req, res) => {
   const doorControler = new DoorController(req, res);
+  arduinoSerialPort.write("o");
   doorControler.open();
 });
 
 app.post("/close/:doorID", (req, res) => {
   const doorControler = new DoorController(req, res);
+  arduinoSerialPort.write("c");
   doorControler.close();
 });
 
